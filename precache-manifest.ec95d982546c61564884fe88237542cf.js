e50091a5af2bee7d82e8d417f8e0aebf5c56a3ca
Ariegarda <djkulmin@gmail.com>
2020-06-19 13:34:39 +0200

    Updates

---
+++ precache-manifest.ec95d982546c61564884fe88237542cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d08ec674ffe391f4620b434081790c1",
+    "url": "/aplikacja/index.html"
+  },
+  {
+    "revision": "c419c6fa99606d367659",
+    "url": "/aplikacja/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "32db7df0d3731baa92f2",
+    "url": "/aplikacja/static/css/main.58a9ea37.chunk.css"
+  },
+  {
+    "revision": "c419c6fa99606d367659",
+    "url": "/aplikacja/static/js/2.f354e1ae.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/aplikacja/static/js/2.f354e1ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32db7df0d3731baa92f2",
+    "url": "/aplikacja/static/js/main.5a2467bd.chunk.js"
+  },
+  {
+    "revision": "1eabc30b97d22c4a48d6",
+    "url": "/aplikacja/static/js/runtime-main.b8b3c3b3.js"
+  }
+]);
